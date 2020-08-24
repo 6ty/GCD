c3f674112cefb2882c513fc626490ced1d828831
Arturo Ivo <t27742664@gmail.com>
2020-08-24 15:58:36 +0300

    20200804_15_51

---
+++ GCD/gcd.py
@@ -0,0 +1,9 @@
+#Import math Library
+import math
+
+#find the highest number that can divide two numbers
+print (math.gcd(26, 12))
+print (math.gcd(12, 6))
+print (math.gcd(10, 0))
+print (math.gcd(0, 34))
+print (math.gcd(0, 0))
