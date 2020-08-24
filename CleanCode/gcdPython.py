36a622140e9805c3061810efb6fb51f040f007c3
Artūrs Ivaņutenko <69342895+6ty@users.noreply.github.com>
2020-08-24 18:27:19 +0300

    Update gcdPython.py

--- CleanCode/gcdPython.py
+++ CleanCode/gcdPython.py
@@ -2,8 +2,8 @@
 import math
 
 #find the highest number that can divide two numbers
-print (math.gcd(26, 12))
-print (math.gcd(12, 6))
-print (math.gcd(10, 0))
-print (math.gcd(0, 34))
-print (math.gcd(0, 0))
+print (math.gcd(26, 12)) #=>2
+print (math.gcd(12, 6)) #=>6
+print (math.gcd(10, 0)) #=>10
+print (math.gcd(0, 34)) #=>34
+print (math.gcd(0, 0)) #=>0
